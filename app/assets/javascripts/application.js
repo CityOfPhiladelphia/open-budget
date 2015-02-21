798762627085341d5f82db72d87c7f3310509aa3
timwis <tim@timwis.com>
2015-02-21 15:30:18 -0500

    Added gulpfile, removed application.js

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require underscore
-//= require d3/d3.v2.min
-//= require bootstrap
-//= require tooltip
-//= require table
-//= require nodes
-//= require visualisation
-//= require init
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+	minifyCss = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin'),
+	del = require('del');
+
+var dir = {
+	dev: './app/',
+	prod: './public/'
+};
+
+/**
+ * Main execution
+ */
+gulp.task('default', ['clean'], function() {
+	gulp.start('usemin', 'images', 'data');
+});
+
+/**
+ * Usemin
+ * A multi-task - parses HTML docs and applies supplied functions to any defined JS/CSS blocks
+ */
+gulp.task('usemin', function() {
+	return gulp.src(dir.dev + '*.html')
+		.pipe(usemin({
+			css: [minifyCss(), 'concat'],
+			js: [uglify()]
+		}))
+		.pipe(gulp.dest(dir.prod));
+});
+
+/**
+ * Images
+ * Copies images directory to production folder
+ */
+gulp.task("images", function() {
+    return gulp.src(dir.dev + "assets/images/**/*")
+        .pipe(gulp.dest(dir.prod + "assets/images"));
+});
+
+/**
+ * Data
+ * Copies data directory to production folder
+ */
+gulp.task("data", function() {
+    return gulp.src(dir.dev + "data/**/*")
+        .pipe(gulp.dest(dir.prod + "data"));
+});
+
+/**
+ * Clean
+ * Cleans out the prod/build directory for a new compile
+ */
+gulp.task('clean', function(cb) {
+	return del(dir.prod, cb);
+});
